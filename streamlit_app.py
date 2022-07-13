b8c0832b4d49d3e11e2cdb5172dcf9aa084de64d
Marvin Osorio <109244474+marvosorio@users.noreply.github.com>
2022-07-13 14:54:42 -0500

    Update streamlit_app.py
    Changed Language, added emojis and added new menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,11 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 2 & Blueberry Outmeal')
-streamlit.text('Kale spinach and Rocket smoothie')
-streamlit.text('Hard-Boiled Free-Range egg')
+
+
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 2 & Blueberry Outmeal')
+streamlit.text('🥗 Kale spinach and Rocket smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range egg')
+streamlit.text('🥑🍞 Avocado Toast')
               
