70a885afe73c0c1e01e46f40212f6ccccf129ac6
Marvin Osorio <109244474+marvosorio@users.noreply.github.com>
2022-07-13 14:51:57 -0500

    Update streamlit_app.py
    added menu items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 2 & Blueberry Outmeal')
+streamlit.text('Kale spinach and Rocket smoothie')
+streamlit.text('Hard-Boiled Free-Range egg')
+              
